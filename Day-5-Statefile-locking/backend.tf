d4582e3ba3b2458c4918361b85a3ece99613249f
Sukumar <purushotham.yendrathi@gmail.com>
2026-09-18 23:58:51 +0800

    Day-5-statelocking

--- Day-5-Statefile-locking/backend.tf
+++ Day-5-Statefile-locking/backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "mybucketofpurush"
         key    = "terraform.tfstate"
         region = "us-east-1"
-        #use_lockfile = true #supports terrafrom latest version >=1.10
+        use_lockfile = true #supports terrafrom latest version >=1.10
         #dynamodb_table = "tf-state-locking" #supports terrafrom any version 
     }
 }
